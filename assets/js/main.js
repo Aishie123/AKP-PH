8d438d16a2c47283ef95f512d2daa972534b4441
“Aishie123” <aishalimdones@gmail.com>
2024-03-04 01:12:24 +0800

    added cursor effect

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,42 +1,28 @@
-/*==================== GOOGLE MAPS ====================*/
-// Initialize and add the map
-function initMap() {
-    const myLatLng = { lat: 14.561393895081313, lng: 121.10272999899031 };
-    const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 8,
-        center: myLatLng,
-    });
+/*==================== CURSOR ====================*/
+const cursor = document.querySelector(".cursor");
+var timeout;
 
-    // Create a marker with animation
-    const marker = new google.maps.Marker({
-        position: myLatLng,
-        map: map,
-        title: "Hello World!",
-        animation: google.maps.Animation.DROP, // You can use BOUNCE for a bouncing effect
-    });
+//follow cursor on mousemove
+document. addEventListener ("mousemove", (e) => {
+    let x = e.clientX;
+    let y = e.clientY;
 
-    // Set up a scroll event listener
-    window.addEventListener("scroll", function () {
-        // Check if the map is in the viewport
-        if (isInViewport(map.getDiv())) {
-            // Trigger the marker animation
-            marker.setAnimation(google.maps.Animation.BOUNCE);
-        }
-    });
-}
+    cursor.style.top = y + "px";
+    cursor.style.left = x + "px";
+    cursor.style.display = "block";
 
-// Helper function to check if an element is in the viewport
-function isInViewport(element) {
-    const rect = element.getBoundingClientRect();
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <=
-            (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <=
-            (window.innerWidth || document.documentElement.clientWidth)
-    );
-}
+    //cursor effects on mouse stopped
+    function mouseStopped(){
+        cursor.style.display = "none";
+    }
+    clearTimeout(timeout);
+    timeout = setTimeout(mouseStopped, 2000);
+});
+
+//cursor effects on mouseout
+document.addEventListener("mouseout", () => {
+    cursor.style.display = "none";
+});
 
 /*==================== LOADER ====================*/
 window.addEventListener("load", function () {
